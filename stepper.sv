dfbc0322d7273824f630db7f0c6088730f950f34
letrend <st@gi.ai>
2020-11-15 23:33:39 +0100

    inital

--- stepper.sv
+++ stepper.sv
@@ -2,15 +2,16 @@ module Stepper (
     input clk,
     input reset,
     input write,
+    input [1:0] address,
     input unsigned [31:0] writedata,
     input read,
     output unsigned [31:0] readdata,
     output step,
-    output dir
+    output dir,
+    output reg enable
   );
 
   parameter CLOCK_FREQ_HZ = 50_000_000;
-  parameter STEP_FREQ_HZ = 10_000_000;
 
   reg waitFlag;
   assign waitrequest = (waitFlag && (read||write));
@@ -19,6 +20,7 @@ module Stepper (
 
   reg slow_clk;
   integer clk_counter;
+  integer step_freq_hz;
 
   assign step = slow_clk;
 
@@ -29,7 +31,7 @@ module Stepper (
     end else begin
       if(state==STEP)begin
         if(clk_counter==0)begin
-          clk_counter <= CLOCK_FREQ_HZ/STEP_FREQ_HZ/2-1;
+          clk_counter <= CLOCK_FREQ_HZ/step_freq_hz/2-1;
           slow_clk <= !slow_clk;
         end else begin
           clk_counter <= clk_counter - 1;
@@ -58,13 +60,23 @@ module Stepper (
   assign state = (step_counter!=target_steps?STEP:IDLE);
   assign readdata = step_counter;
 
-  always @ ( posedge clk ) begin: AVALON_INTERFACE
-    if(step_counter==0)begin // wait for it to apply
-      reset_step_counter <= 0;
-    end
-    if(write)begin
-      reset_step_counter <= 1;
-      target_steps <= writedata;
+  always @ ( posedge reset, posedge clk ) begin: AVALON_INTERFACE
+    if(reset)begin
+      step_freq_hz <= 16_000;
+    end else begin
+      if(step_counter==0)begin // wait for it to apply
+        reset_step_counter <= 0;
+      end
+      if(write)begin
+        if(address==0)begin
+          reset_step_counter <= 1;
+          target_steps <= writedata;
+        end else if (address==1)begin
+          step_freq_hz <= writedata;
+        end else if (address==2)begin
+          enable <= writedata;
+        end
+      end
     end
   end
 
